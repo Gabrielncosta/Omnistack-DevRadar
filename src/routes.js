42143a71fea4903c6981fc669bc66f9bffb836a4
Gabriel <>
2020-02-04 13:31:52 -0300

    github api

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,21 @@
 const { Router } = require('express');
+const axios = require('axios');
 
 const routes = Router();
 
-routes.post('/users', (req, res) => {
-    console.log(req.body);
-    res.json({hello: 'world'});
+routes.post('/devs', async (req, res) => {
+    const { github_username, techs } = req.body;
+
+    const response = await axios.get(`https://api.github.com/users/${github_username}`)
+
+    const { name = login, avatar_url, bio } = response.data;
+
+    console.log(name, avatar_url, bio, github_username);
+
+    const techsArray = techs.split(',').map(tech => tech.trim());
+
+    
+    res.json({hello: response.data });
 });
 
 module.exports = routes;
