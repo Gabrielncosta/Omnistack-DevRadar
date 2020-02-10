b0264cb04785bebe91dab9ea869afeab097a39ed
Gabrielncosta <gabrielncosta208@gmail.com>
2020-02-09 21:12:34 -0300

    geolocation

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,33 +1,53 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css';
 
 function App() {
+  const [latitude, setLatitude] = useState(''),
+  const [longitude, setLongitude] = useState(''),
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    )
+  }, []);
+
   return (
-    <div className="App">
+    <div id="app">
       <aside>
         <strong>Cadastrar</strong>
           <form>
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="github_username">Usuário do Github</label> 
               <input name="github_username" id="github_username" required />
             </div>
             
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="techs">Técnologias</label> 
               <input name="techs" id="techs" required />
             </div>
             
             <div className="input-group">
-              <div class="input-block">
+              <div className="input-block">
                 <label htmlFor="latitude">Latitude</label> 
                 <input name="latitude" id="latitude" required />
               </div>
 
-              <div class="input-block">
+              <div className="input-block">
                 <label htmlFor="longitude">Longitude</label> 
                 <input name="longitude" id="longitude" required />
               </div>
@@ -37,6 +57,52 @@ function App() {
           </form>
       </aside>
       <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4" alt=""/>
+              <div className="user-info">
+                <strong>dasdsa</strong>
+                <span>dsadas</span>
+              </div>
+            </header>
+            <p>descrição</p>
+            <a href="www.google.com">Acessar perfil no GitHub</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4" alt=""/>
+              <div className="user-info">
+                <strong>dasdsa</strong>
+                <span>dsadas</span>
+              </div>
+            </header>
+            <p></p>
+            <a href="www.google.com">Acessar perfil no GitHub</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4" alt=""/>
+              <div className="user-info">
+                <strong>dasdsa</strong>
+                <span>dsadas</span>
+              </div>
+            </header>
+            <p></p>
+            <a href="www.google.com">Acessar perfil no GitHub</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4" alt=""/>
+              <div className="user-info">
+                <strong>dasdsa</strong>
+                <span>dsadas</span>
+              </div>
+            </header>
+            <p></p>
+            <a href="www.google.com">Acessar perfil no GitHub</a>
+          </li>
+        </ul>
       </main>
     </div>
   );
